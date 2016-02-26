cd82d2978630c47ace7889303dd0ff87e14c1540
Andrew Sodt <andrews@alleninstitute.org>
2016-02-26 15:17:45 -0800

    add function dispatch

--- websocket/server.py
+++ websocket/server.py
@@ -5,6 +5,8 @@ from twisted.python import log
 from twisted.web.server import Site
 from twisted.web.static import File
 import numpy as np
+import pgmagick
+import argparse
 
 from autobahn.twisted.websocket import WebSocketServerFactory, \
     WebSocketServerProtocol
@@ -14,16 +16,36 @@ from autobahn.twisted.resource import WebSocketResource
 
 class DatacubeProtocol(WebSocketServerProtocol):
 
+    def __init__(self):
+        # This is a dispatch table of functions that respond to the call request
+        self.dispatch = {   'cube': self.cube,
+                            'jp2': self.jp2
+        }
+
+        # This is the argument parser for all the functions
+        self.parser = argparse.ArgumentParser(prog='Datacube')
+        self.parser.add_argument('call', nargs='?')
+
     def onConnect(self, request):
         print("WebSocket connection request: {}".format(request))
 
     def onMessage(self, payload, isBinary):
-        for i in range(10):
-             self.sendMessage(data[i].tobytes(), True)
+        # parse the request
+        request = self.parser.parse_args(payload.decode('utf8').split())
+        # dispatch to the function
+        self.dispatch[request.call](request)
 
+    # dispatch functions
+
+    # send cube data
+    def cube(self, request):
+        self.sendMessage(data[1].tobytes(), isBinary=True)
+
+
+    def jp2(self, request):
+        self.sendMessage(data[1].tobytes(), isBinary=True)
 
 if __name__ == '__main__':
-
     log.startLogging(sys.stdout)
 
     # load cell types data
