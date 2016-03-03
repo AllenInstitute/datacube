6795b2eb292e505a00b17444e01bbaa1c9d62fcc
Andrew Sodt <andrews@alleninstitute.org>
2016-03-03 13:16:06 -0800

    add a demo heatmap page
    Former-commit-id: 127c062ee9e492d169a4eb970eabb43c8a6ec898

--- websocket/server.py
+++ websocket/server.py
@@ -2,8 +2,12 @@ import sys
 
 from twisted.internet import reactor
 from twisted.python import log
+from pprint import pprint
+from twisted.application.internet import TCPServer
+from twisted.application.service import Application
 from twisted.web.server import Site
 from twisted.web.static import File
+from twisted.web.resource import Resource
 import numpy as np
 import argparse
 
@@ -44,6 +48,19 @@ class DatacubeProtocol(WebSocketServerProtocol):
     def jp2(self, request):
         self.sendMessage(data[1].tobytes(), isBinary=True)
 
+class TsvPage(Resource):
+    def render_GET(self, request):
+        return self.process(request)
+
+    def render_POST(self, request):
+        return self.process(request)
+
+    def process(self, request):
+        pprint(request.__dict__)
+        newdata = request.content.getvalue()
+        print newdata
+        return ''
+
 if __name__ == '__main__':
     log.startLogging(sys.stdout)
 
@@ -61,6 +78,9 @@ if __name__ == '__main__':
 
     # and our WebSocket server under "/ws"
     root.putChild(u"ws", resource)
+
+    # respond to tab separated value request under "/tsv"
+    root.putChild(u"tsv", TsvPage())
 
     # both under one Twisted Web Site
     site = Site(root)
