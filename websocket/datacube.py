03e2a6607880acdd4f57e577b7a4557e057da8e9
Chris Barber <barberchris01@gmail.com>
2016-03-03 11:31:14 -0800

    add datacube class with correlation search method

---
+++ websocket/datacube.py
@@ -0,0 +1,149 @@
+import numpy as np
+import scipy as sp
+import distarray
+from distarray.globalapi import Context, Distribution
+
+def correlation_search(data, seed, axis, query=None, mdata=None, mseed=None):
+    import numpy as np
+    
+    if query is None:
+        query = [np.ones(1)]*data.ndim
+    
+    mask_args = \
+        lambda axes=range(0,data.ndim), out_axes=[axis], more=[]: \
+            list(sum([(mask.squeeze(),[i]) for i, mask in enumerate(query) if i in axes and mask.size>1], ()))+more+[out_axes]
+    
+    sdims = range(0,seed.ndim)
+    ddims = range(0,data.ndim)
+    sample_axes = np.array([i for i in ddims if i != axis])
+
+    if mdata is None:
+        seed_num_samples = np.prod(np.array([query[i].sum() if query[i].size>1 else data.shape[i] for i in sample_axes]))
+        num_samples = seed_num_samples
+        mseed_args = []
+        mdata_args = []
+    else:
+        seed_num_samples = np.einsum(mseed, range(0,mseed.ndim), *mask_args(sample_axes, []))
+        num_samples = np.einsum(mdata, range(0,mdata.ndim), *mask_args())
+        mseed_args = [mseed, range(0, mseed.ndim)]
+        mdata_args = [mdata, range(0, mdata.ndim)]
+
+    seed_mean = np.einsum(seed, sdims, *mask_args(sample_axes, [], mseed_args)) / seed_num_samples
+    data_mean = np.einsum(data, ddims, *mask_args(more=mdata_args)) / num_samples
+    data_mean.shape = tuple(data.shape[i] if i == axis else 1 for i in ddims) # restore dims after einsum
+    
+    seed_dev = np.einsum(seed-seed_mean, sdims, *mask_args(sample_axes, sdims, mseed_args))
+    numerator = np.einsum(seed_dev, ddims, data, ddims, query[axis].flat, [axis], *mask_args((), [axis], mdata_args))
+    numerator -= np.einsum(seed_dev, ddims, data_mean*query[axis], ddims, [axis])
+
+    denominator = np.einsum(data, ddims, data, ddims, *mask_args(more=mdata_args))
+    # data_mean has already been masked
+    denominator += -2.0*np.einsum(data, ddims, data_mean*query[axis], ddims, *mask_args(sample_axes, more=mdata_args))
+    denominator += np.sum(data_mean**2, axis=sample_axes) * num_samples
+    denominator *= np.einsum(seed_dev**2, sdims, *mask_args(sample_axes))
+    denominator = np.sqrt(denominator)
+
+    return np.clip(numerator / denominator, -1.0, 1.0) / query[axis].flat # intentional divide by zero
+
+
+def _local_search(search_function, darr, seed, axis, global_query, marr, mseed):
+    import numpy as np
+
+    query, data, mdata, empty = _get_local_query(darr, marr, global_query)
+    if len(empty)>0:
+        if axis in empty:
+            return np.array([], dtype=darr.ndarray.dtype)
+        else:
+            result = np.empty((darr.local_shape[axis]), dtype=darr.ndarray.dtype)
+            result.fill(np.nan)
+            return result
+
+    return globals()[search_function](data, seed, axis, query, mdata, mseed)
+
+
+def _get_local_query(darr, marr, global_query):
+    import numpy as np
+    assert(len(global_query)==darr.ndim)
+    
+    local_query = [np.ones(1)]*darr.ndim
+    local_subscripts = [range(0,darr.local_shape[axis]) for axis in range(0,darr.ndim)]
+    subscripted = False
+    empty = []
+    for axis, mask in enumerate(global_query):
+        if isinstance(mask, np.ndarray):
+            if mask.dtype == np.bool and mask.size>0:
+                local_query[axis] = mask[darr.distribution._maps[axis].global_slice].astype(np.float32)
+            elif np.issubdtype(mask.dtype, np.integer):
+                subscripted = True
+                local_indices = []
+                for global_index in mask:
+                    try:
+                        local_index = darr.distribution._maps[axis].local_from_global_index(global_index)
+                        local_indices.append(local_index)
+                    except IndexError:
+                        continue
+                
+                if len(local_indices)>0:
+                    local_subscripts[axis] = local_indices
+                else:
+                    empty.append(axis)
+            else:
+                raise IndexError
+
+    mdata = None
+    if len(empty)>0:
+        data = None
+    elif subscripted:
+        data = darr.ndarray[np.ix_(*local_subscripts)]
+        if marr is not None:
+            mdata = marr.ndarray[np.ix_(*local_subscripts)]
+    else:
+        data = darr.ndarray
+        if marr is not None:
+            mdata = marr.ndarray
+
+    return local_query, data, mdata, empty
+
+
+class Datacube:
+    def __init__(self, data, observed=None, distribution=None):
+        self.context = Context()
+        assert(isinstance(data, np.ndarray))
+        self.data = data
+        if observed is not None:
+            assert(isinstance(observed, np.ndarray))
+            assert(observed.shape == data.shape)
+            assert(observed.dtype == data.dtype)
+        self.observed = observed
+        if distribution is not None:
+            assert(len(distribution) == data.ndim)
+            self.distribution = Distribution(self.context, self.data.shape, dist=distribution)
+        else:
+            # default to block-distributed on first axis and not distributed otherwise
+            self.distribution = Distribution(self.context, self.data.shape, tuple('b' if i == 0 else 'n' for i in range(0,data.ndim)))
+
+        self.dist_data = self.context.fromarray(self.data, self.distribution)
+        if self.observed is not None:
+            self.dist_observed = self.context.fromarray(self.observed, self.distribution)
+        else:
+            self.dist_observed = None
+
+        self.context.push_function(_get_local_query.__name__, _get_local_query)
+        self.context.push_function(correlation_search.__name__, correlation_search)
+        
+    def get_correlated(self, seed_index, axis, query=None):
+        if query is None:
+            query = [None]*self.dist_data.ndim
+
+        seed = self.data.take([seed_index], axis=axis)
+        for i, mask in enumerate(query):
+            if i != axis and mask is not None and mask.dtype != np.bool and np.issubdtype(mask.dtype, np.integer):
+                seed = seed.take(mask, axis=i)
+        
+        seed_observed = None
+        if self.observed is not None:
+            seed_observed = self.observed.take([seed_index], axis=axis)
+        
+        dist_observed_key = self.dist_observed.key if self.dist_observed is not None else None
+        args = (correlation_search.__name__, self.dist_data.key, seed, 0, query, dist_observed_key, seed_observed)
+        return np.concatenate(self.dist_data.context.apply(_local_search, args))
