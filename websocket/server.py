b49735bf7c796a178d23c478d13c801e49c5e2b4
Chris Barber <barberchris01@gmail.com>
2016-03-04 16:16:31 -0800

    api to select part of heatmap, api for correlation search, render to canvas
    Former-commit-id: 0851963ada2937bbae42d8b596af201cce038dc9

--- websocket/server.py
+++ websocket/server.py
@@ -9,7 +9,11 @@ from twisted.web.server import Site
 from twisted.web.static import File
 from twisted.web.resource import Resource
 import numpy as np
+import scipy as sp
+from scipy import stats
 import argparse
+import json
+import struct
 from datacube import Datacube
 
 from autobahn.twisted.websocket import WebSocketServerFactory, \
@@ -23,33 +27,53 @@ class DatacubeProtocol(WebSocketServerProtocol):
     def __init__(self):
         # This is a dispatch table of functions that respond to the call request
         self.dispatch = {   'cube': self.cube,
-                            'jp2': self.jp2
+                            'jp2': self.jp2,
+                            'corr': self.corr
         }
-
-        # This is the argument parser for all the functions
-        self.parser = argparse.ArgumentParser(prog='Datacube')
-        self.parser.add_argument('call', nargs='?')
 
     def onConnect(self, request):
         print("WebSocket connection request: {}".format(request))
 
     def onMessage(self, payload, isBinary):
-        r=datacube.get_correlated(10, 0)
-        print(np.nanmean(r))
+        print(np.max(data))
         # parse the request
-        request = self.parser.parse_args(payload.decode('utf8').split())
+        request = json.loads(payload.decode('utf8'))
         # dispatch to the function
-        self.dispatch[request.call](request)
+        self.dispatch[request['call']](request)
 
     # dispatch functions
 
     # send cube data
     def cube(self, request):
-        self.sendMessage(data[1].tobytes(), isBinary=True)
+        subscripts = [slice(None,None,None)]*data.ndim
+        if 'select' in request:
+            assert(isinstance(request['select'], list))
+            assert(len(request['select']) == data.ndim)
+            for axis, selector in enumerate(request['select']):
+                if isinstance(selector, list):
+                    if isinstance(selector[0], int):
+                        subscripts[axis] = np.array(selector, dtype=np.int)
+                    elif isintance(selector[0], bool):
+                        subscripts[axis] = np.array(selector, dtype=np.bool)
+                elif isinstance(selector, dict):
+                    if 'step' in selector:
+                        subscripts[axis] = slice(selector['start'], selector['stop'], selector['step'])
+                    else:
+                        subscripts[axis] = slice(selector['start'], selector['stop'], None)
+
+        shape = data[subscripts].shape
+        msg = struct.pack('>I', shape[1],) + struct.pack('>I', shape[0]) + zscore[subscripts].tobytes()
+        self.sendMessage(msg, isBinary=True)
 
 
     def jp2(self, request):
         self.sendMessage(data[1].tobytes(), isBinary=True)
+
+    def corr(self, request):
+        r=datacube.get_correlated(request['seed'], 0)
+        print(np.nanmean(r))
+        self.sendMessage(json.dumps(np.argsort(-r).tolist()))
+
 
 class TsvPage(Resource):
     def render_GET(self, request):
@@ -68,7 +92,8 @@ if __name__ == '__main__':
     log.startLogging(sys.stdout)
 
     # load cell types data
-    data = np.load('../data/ivscc.npy')
+    data = np.load('../data/ivscc.npy').astype(np.float32)
+    zscore = sp.stats.mstats.zscore(data, axis=1)
     print("data ", data[0])
 
     datacube = Datacube(data)
