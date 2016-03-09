74723feaae686b93210b87145c35f2accbb5b293
Andrew Sodt <andrews@alleninstitute.org>
2016-03-09 13:43:50 -0800

    broke out dispatch code

--- websocket/server.py
+++ websocket/server.py
@@ -3,18 +3,13 @@ import sys
 from twisted.internet import reactor
 from twisted.python import log
 from pprint import pprint
-from twisted.application.internet import TCPServer
-from twisted.application.service import Application
 from twisted.web.server import Site
 from twisted.web.static import File
 from twisted.web.resource import Resource
 import numpy as np
-import scipy as sp
-from scipy import stats
-import argparse
 import json
-import struct
 from datacube import Datacube
+from dispatch import Dispatch
 
 from autobahn.twisted.websocket import WebSocketServerFactory, \
     WebSocketServerProtocol
@@ -22,59 +17,19 @@ from autobahn.twisted.websocket import WebSocketServerFactory, \
 from autobahn.twisted.resource import WebSocketResource
 
 
+# Responds to web socket messages with json payloads
 class DatacubeProtocol(WebSocketServerProtocol):
-
-    def __init__(self):
-        # This is a dispatch table of functions that respond to the call request
-        self.dispatch = {   'cube': self.cube,
-                            'jp2': self.jp2,
-                            'corr': self.corr
-        }
-
     def onConnect(self, request):
         print("WebSocket connection request: {}".format(request))
 
     def onMessage(self, payload, isBinary):
-        print(np.max(data))
         # parse the request
         request = json.loads(payload.decode('utf8'))
         # dispatch to the function
-        self.dispatch[request['call']](request)
-
-    # dispatch functions
-
-    # send cube data
-    def cube(self, request):
-        subscripts = [slice(None,None,None)]*data.ndim
-        if 'select' in request:
-            assert(isinstance(request['select'], list))
-            assert(len(request['select']) == data.ndim)
-            for axis, selector in enumerate(request['select']):
-                if isinstance(selector, list):
-                    if isinstance(selector[0], int):
-                        subscripts[axis] = np.array(selector, dtype=np.int)
-                    elif isintance(selector[0], bool):
-                        subscripts[axis] = np.array(selector, dtype=np.bool)
-                elif isinstance(selector, dict):
-                    if 'step' in selector:
-                        subscripts[axis] = slice(selector['start'], selector['stop'], selector['step'])
-                    else:
-                        subscripts[axis] = slice(selector['start'], selector['stop'], None)
-
-        shape = data[subscripts].shape
-        msg = struct.pack('>I', shape[1],) + struct.pack('>I', shape[0]) + zscore[subscripts].tobytes()
-        self.sendMessage(msg, isBinary=True)
+        self.sendMessage(dispatch.functions[request['call']](request), dispatch.isBinary[request['call']])
 
 
-    def jp2(self, request):
-        self.sendMessage(data[1].tobytes(), isBinary=True)
-
-    def corr(self, request):
-        r=datacube.get_correlated(request['seed'], 0)
-        print(np.nanmean(r))
-        self.sendMessage(json.dumps(np.argsort(-r).tolist()))
-
-
+# Responds to https requests with json payloads
 class TsvPage(Resource):
     def render_GET(self, request):
         return self.process(request)
@@ -93,10 +48,8 @@ if __name__ == '__main__':
 
     # load cell types data
     data = np.load('../data/ivscc.npy').astype(np.float32)
-    zscore = sp.stats.mstats.zscore(data, axis=1)
-    print("data ", data[0])
-
     datacube = Datacube(data)
+    dispatch = Dispatch(datacube)
 
     factory = WebSocketServerFactory(u"ws://127.0.0.1:9000")
     factory.protocol = DatacubeProtocol
