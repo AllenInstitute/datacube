8593ae10aa1383bceca5e05e43e21c29c39fc952
Chris Barber <chrisba@alleninstitute.org>
2017-04-19 14:29:43 -0700

    PBS-146 add lib/es2015, publish to npm

--- clients/build.sh
+++ clients/build.sh
@@ -1,3 +1,4 @@
+babel src -d lib/es2015
 babel src -d lib/amd --plugins transform-es2015-modules-amd
 babel src -d lib/commonjs --plugins transform-es2015-modules-commonjs
 babel src -d lib/systemjs --plugins transform-es2015-modules-systemjs
