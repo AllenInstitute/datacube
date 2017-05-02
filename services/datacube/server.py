4ea8604f40275ebaa98a08442e2a88e36abf1ba2
Chris Barber <chrisba@alleninstitute.org>
2017-05-02 14:35:56 -0700

    PBS-341 reference space demo

--- services/datacube/server.py
+++ services/datacube/server.py
@@ -2,6 +2,7 @@ import txaio
 from pprint import pprint
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
+from autobahn.wamp.types import RegisterOptions
 import numpy as np
 import json
 import base64
@@ -10,7 +11,8 @@ import argparse
 import pg8000
 import os
 import sys
-from progressbar import ProgressBar, Percentage, Bar, ETA, Counter, FileTransferSpeed
+import StringIO
+from PIL import Image
 
 from datacube import Datacube
 #import cam
@@ -68,6 +70,31 @@ class DatacubeComponent(ApplicationSession):
             except Exception as e:
                 self._format_error(e)
 
+        def image(select, image_format='jpeg', cube=None):
+            try:
+                cube = self._select_cube(cube)
+                data = datacube[cube].raw(select)
+                data = np.squeeze(data)
+
+                if data.ndim != 2 and not (data.ndim == 3 and data.shape[2] == 4):
+                    raise error.Non2dImageError()
+
+                if data.ndim == 2 and data.dtype != np.uint8:
+                    data = ((data / datacube[cube].max) * 255.0).astype(np.uint8)
+
+                image = Image.fromarray(data)
+                buf = StringIO.StringIO()
+                if image_format.lower() == 'jpeg':
+                    image.save(buf, format='JPEG', quality=40)
+                elif image_format.lower() == 'png':
+                    image.save(buf, format='PNG')
+                else:
+                    raise error.InvalidOrUnsupportedImageFormat()
+                #return {'data': bytes(buf.getvalue())}
+                return {'data': 'data:image/' + image_format.lower() + ';base64,' + base64.b64encode(buf.getvalue())}
+            except Exception as e:
+                self._format_error(e)
+
         def corr(select, axis, seed, cube=None):
             try:
                 cube = self._select_cube(cube)
@@ -109,6 +136,7 @@ class DatacubeComponent(ApplicationSession):
             yield self.register(raw, u'org.alleninstitute.datacube.raw')
             yield self.register(log2_1p, u'org.alleninstitute.datacube.log2_1p')
             yield self.register(standard, u'org.alleninstitute.datacube.standard')
+            yield self.register(image, u'org.alleninstitute.datacube.image')
             yield self.register(info, u'org.alleninstitute.datacube.info')
             yield self.register(corr, u'org.alleninstitute.datacube.corr')
             yield self.register(fold_change, u'org.alleninstitute.datacube.fold_change')
@@ -135,12 +163,13 @@ class DatacubeComponent(ApplicationSession):
         ndim = data.ndim
         shape = data.shape
         big_endian = data.byteswap()
-        enc_data = base64.b64encode(big_endian.tobytes())
-        return {'ndim': ndim, 'shape': shape, 'data': enc_data, 'dtype': data.dtype.name}
+        #enc_data = base64.b64encode(big_endian.tobytes())
+        return {'ndim': ndim, 'shape': shape, 'data': bytes(big_endian.tobytes()), 'dtype': unicode(data.dtype.name)}
+
 
     def _format_error(self, e):
         traceback.print_exc()
-        err_dict = {'error': True, 'args': e.args, 'class': e.__class__.__name__, 'doc': e.__doc__, 'message': e.message, 'traceback': traceback.format_exc()}
+        err_dict = {'error': True, 'class': unicode(e.__class__.__name__), 'doc': unicode(e.__doc__), 'message': unicode(e.message), 'traceback': unicode(traceback.format_exc())}
         raise error.UnspecifiedError(err_dict)
 
 
@@ -160,50 +189,52 @@ if __name__ == '__main__':
     datacube = dict()
 
     if args.demo:
-        # load cell types data
-        conn = pg8000.connect(user=DB_USER, host=DB_HOST, port=DB_PORT, database=DB_NAME, password=DB_PASSWORD)
-        cursor = conn.cursor()
-        cursor.execute("select name from data_cube_runs")
-        results = cursor.fetchall()
-        #available_cubes = str(results[0])
+        #from progressbar import ProgressBar, Percentage, Bar, ETA, Counter, FileTransferSpeed
 
-        data = None
-        if not os.path.exists(DATA_DIR + 'cell_types.npy'):
-            print('Loading cell_types datacube from warehouse ...')
+        ## load cell types data
+        #conn = pg8000.connect(user=DB_USER, host=DB_HOST, port=DB_PORT, database=DB_NAME, password=DB_PASSWORD)
+        #cursor = conn.cursor()
+        #cursor.execute("select name from data_cube_runs")
+        #results = cursor.fetchall()
+        ##available_cubes = str(results[0])
 
-            cursor.execute("select array_length(col_ids, 1) from data_cube_runs where name = 'cell_types'")
-            num_cols = cursor.fetchall()[0][0]
+        #data = None
+        #if not os.path.exists(DATA_DIR + 'cell_types.npy'):
+        #    print('Loading cell_types datacube from warehouse ...')
 
-            cursor.execute("""select count(*) from data_cubes dc
-                join data_cube_runs dcr on dc.data_cube_run_id = dcr.id
-                where dcr.name = 'cell_types'""")
-            num_rows = cursor.fetchall()[0][0]
+        #    cursor.execute("select array_length(col_ids, 1) from data_cube_runs where name = 'cell_types'")
+        #    num_cols = cursor.fetchall()[0][0]
 
-            data = np.full((num_rows, num_cols), np.nan, dtype=np.float32)
+        #    cursor.execute("""select count(*) from data_cubes dc
+        #        join data_cube_runs dcr on dc.data_cube_run_id = dcr.id
+        #        where dcr.name = 'cell_types'""")
+        #    num_rows = cursor.fetchall()[0][0]
 
-            cursor.execute("select data from data_cubes dc join data_cube_runs dcr on dc.data_cube_run_id = dcr.id where dcr.name = '%s'" % 'cell_types')
+        #    data = np.full((num_rows, num_cols), np.nan, dtype=np.float32)
 
-            progress = ProgressBar(widgets=[Percentage(), ' ', Bar(), ' ', Counter(), '/' + str(num_rows) + ' rows ', ETA(), ' ', FileTransferSpeed(unit='rows')], maxval=num_rows)
-            progress.start()
+        #    cursor.execute("select data from data_cubes dc join data_cube_runs dcr on dc.data_cube_run_id = dcr.id where dcr.name = '%s'" % 'cell_types')
 
-            BATCH_SIZE=50
-            row_idx = 0
-            while True:
-                chunk = cursor.fetchmany(BATCH_SIZE)
-                if 0 == len(chunk):
-                    break
-                else:
-                    data[row_idx:row_idx+len(chunk),:] = np.asarray(chunk, dtype=np.float32)[:,0]
-                    row_idx += len(chunk)
-                    progress.update(row_idx)
-            progress.finish()
+        #    progress = ProgressBar(widgets=[Percentage(), ' ', Bar(), ' ', Counter(), '/' + str(num_rows) + ' rows ', ETA(), ' ', FileTransferSpeed(unit='rows')], maxval=num_rows)
+        #    progress.start()
 
-            np.save(DATA_DIR + 'cell_types.npy', data)
-        else:
-            print('Loading cached cell_types datacube from ./data/cell_types.npy ...')
-            data = np.load(DATA_DIR + 'cell_types.npy').astype(np.float32)
+        #    BATCH_SIZE=50
+        #    row_idx = 0
+        #    while True:
+        #        chunk = cursor.fetchmany(BATCH_SIZE)
+        #        if 0 == len(chunk):
+        #            break
+        #        else:
+        #            data[row_idx:row_idx+len(chunk),:] = np.asarray(chunk, dtype=np.float32)[:,0]
+        #            row_idx += len(chunk)
+        #            progress.update(row_idx)
+        #    progress.finish()
 
-        datacube['cell_types'] = Datacube(data, distributed=args.distributed, observed=~np.isnan(data))
+        #    np.save(DATA_DIR + 'cell_types.npy', data)
+        #else:
+        #    print('Loading cached cell_types datacube from ./data/cell_types.npy ...')
+        #    data = np.load(DATA_DIR + 'cell_types.npy').astype(np.float32)
+
+        #datacube['cell_types'] = Datacube(data, distributed=args.distributed, observed=~np.isnan(data))
 
         ## load cam data
         #data = None
@@ -216,6 +247,64 @@ if __name__ == '__main__':
         #    data = np.load(DATA_DIR + 'cam.npy').astype(np.float32)
 
         #datacube['cam'] = Datacube(data, distributed=args.distributed, observed=~np.isnan(data))
+
+        # MNI demo
+
+        import nrrd
+        import urllib2
+        from numba import jit
+
+        if not os.path.exists(DATA_DIR + 'ccf.npy'):
+            print('Loading CCF atlas volume from informatics directory')
+            ccf = nrrd.read('/projects/0378/vol1/informatics/model/P56/atlasVolume/average_template_25.nrrd')[0]
+            ccf = (ccf.astype(np.float32)/516.0*255.0).astype(np.uint8)
+            np.save(DATA_DIR + 'ccf.npy', ccf)
+            del ccf
+
+        if not os.path.exists(DATA_DIR + 'ccf_anno.npy') or not os.path.exists(DATA_DIR + 'ccf_anno_color.npy'):
+            print('Loading CCF annotation volume from informatics directory')
+            ccf_anno = nrrd.read('/projects/0378/vol1/informatics/model/P56/atlases/MouseCCF2016/annotation_25.nrrd')[0]
+            np.save(DATA_DIR + 'ccf_anno.npy', ccf_anno)
+
+            print('Generating colorized annotation volume')
+            def get_structure_colors(ccf_anno):
+                annotated_structures = np.unique(ccf_anno)
+                structure_colors = dict()
+                structure_colors[0] = np.array([0, 0, 0, 0], dtype=np.uint8)
+                for structure_id in annotated_structures:
+                    if structure_id:
+                        STRUCTURE_API = 'http' + '://' + 'api.brain-map.org/api/v2/data/Structure/query.json?id=' # todo
+                        color_hex_triplet = json.load(urllib2.urlopen(STRUCTURE_API + str(structure_id)))['msg'][0]['color_hex_triplet']
+                        structure_colors[int(structure_id)] = np.concatenate([np.array(bytearray(color_hex_triplet.decode("hex"))), [255]]).astype(np.uint8)
+                return structure_colors
+                        
+            structure_colors = get_structure_colors(ccf_anno)
+
+            ccf_anno_color = np.zeros(ccf_anno.shape+(4,), dtype=np.uint8)
+
+            @jit(nopython=True)
+            def colorize(ccf_anno, ids, colors, ccf_anno_color):
+                for i in range(ccf_anno.shape[0]):
+                    for j in range(ccf_anno.shape[1]):
+                        for k in range(ccf_anno.shape[2]):
+                            color_idx = np.searchsorted(ids, ccf_anno[i,j,k])
+                            ccf_anno_color[i,j,k,:] = colors[color_idx]
+
+            sorted_colors = sorted(structure_colors.items(), key=lambda x: x[0])
+            ids = map(lambda x: x[0], sorted_colors)
+            colors = np.stack(map(lambda x: x[1], sorted_colors), axis=0)
+            colorize(ccf_anno, ids, colors, ccf_anno_color)
+            np.save(DATA_DIR + 'ccf_anno_color.npy', ccf_anno_color)
+
+            del ccf_anno
+            del ccf_anno_color
+
+        ccf = np.load(DATA_DIR + 'ccf.npy', mmap_mode='r')
+        datacube['ccf'] = Datacube(ccf, distributed=args.distributed)
+        ccf_anno = np.load(DATA_DIR + 'ccf_anno.npy', mmap_mode='r')
+        datacube['ccf_anno'] = Datacube(ccf_anno, distributed=args.distributed)
+        ccf_anno_color = np.load(DATA_DIR + 'ccf_anno_color.npy', mmap_mode='r')
+        datacube['ccf_anno_color'] = Datacube(ccf_anno_color, distributed=args.distributed)
     else:
         print('TODO: implement non-demo mode (load files from data-dir specified on command line)')
         exit(0)
