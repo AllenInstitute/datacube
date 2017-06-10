ffad6189885aa1647e6900ce8f859b592c69482c
Chris Barber <chrisba@alleninstitute.org>
2017-06-09 17:19:42 -0700

    fix router install sha256 check

--- install_router.sh
+++ install_router.sh
@@ -1,5 +1,5 @@
 wget https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-5.8-linux_x86_64-portable.tar.bz2
-sha256sum -c pypy-5.8-linux_x86_64-portable.tar.bz2.sha256sum
+sha256sum -c pypy-5.8-linux_x86_64-portable.tar.bz2.sha256 || exit 1
 tar xvjf pypy-5.8-linux_x86_64-portable.tar.bz2
 pypy-5.8-linux_x86_64-portable/bin/pypy -m ensurepip
 pypy-5.8-linux_x86_64-portable/bin/pypy -m pip install -U pip
