e4f1b0706faac2aa714452a702ee63ab51cf5733
Chris Barber <chrisba@alleninstitute.org>
2017-06-12 10:58:39 -0700

    use latest crossbar for router

--- install_router.sh
+++ install_router.sh
@@ -3,5 +3,5 @@ sha256sum -c pypy-5.8-linux_x86_64-portable.tar.bz2.sha256 || exit 1
 tar xvjf pypy-5.8-linux_x86_64-portable.tar.bz2
 pypy-5.8-linux_x86_64-portable/bin/pypy -m ensurepip
 pypy-5.8-linux_x86_64-portable/bin/pypy -m pip install -U pip
-pypy-5.8-linux_x86_64-portable/bin/pip install `grep crossbar requirements.txt`
+pypy-5.8-linux_x86_64-portable/bin/pip install crossbar
 pypy-5.8-linux_x86_64-portable/bin/crossbar version
