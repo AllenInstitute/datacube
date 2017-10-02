f9f4ac69c6932984c2b1981cd7e4ea17f407df8d
Chris Barber <chrisba@alleninstitute.org>
2017-10-02 13:40:54 -0700

    ; fix config name

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-pypy-5.8-linux_x86_64-portable/bin/crossbar start --logtofile --logdir=log/ --config production.json
+pypy-5.8-linux_x86_64-portable/bin/crossbar start --logtofile --logdir=log/ --config config-prod.json
