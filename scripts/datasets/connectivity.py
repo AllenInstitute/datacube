bc945cdabce16b2dab2b75b68affba0bac8c1dfc
nilegraddis <nilegraddis@gmail.com>
2018-05-04 13:12:49 -0700

    PRODDEV-981 move nested function definitions to top level

--- scripts/datasets/connectivity.py
+++ scripts/datasets/connectivity.py
@@ -21,6 +21,121 @@ from allensdk.config.manifest import Manifest
 
 FIBER_TRACTS_ID = 1009
 SUMMARY_SET_ID = 167587189
+HEMISPHERE_IDS = [1, 2, 3]
+HEMISPHERE_MAP = {1: 'left', 2: 'right', 3: 'bilateral'}
+
+
+def get_projection_table(unionizes, experiment_ids, structure_ids, data_field):
+    unionizes = unionizes.pivot(index='experiment_id', columns='structure_id', values=data_field)
+    unionizes = unionizes.reindex(index=experiment_ids, columns=structure_ids, fill_value=0.0)
+    unionizes = unionizes.fillna(0.0)
+    return unionizes
+
+
+def get_specified_projection_table(mcc, unionizes, experiment_ids, structure_ids,
+                                    is_injection, hemisphere_id, data_field):
+    unionizes = mcc.filter_structure_unionizes(unionizes, is_injection=bool(is_injection),
+                                                structure_ids=structure_ids, include_descendants=True,
+                                                hemisphere_ids=[hemisphere_id])
+    return get_projection_table(unionizes, experiment_ids, structure_ids, data_field)
+
+
+def get_all_unionizes(mcc, all_unionizes_path, experiment_ids):
+    try:
+        unionizes = pd.read_csv(all_unionizes_path)
+    except (IOError, ValueError) as err:
+        unionizes = mcc.get_structure_unionizes(experiment_ids)
+        unionizes.to_csv(all_unionizes_path)
+    return unionizes
+
+
+def map_hemisphere_id(hem_id):
+    return HEMISPHERE_MAP[hem_id]
+
+
+def make_unionize_tables(data_field_key):
+
+    unionizes = get_all_unionizes(mcc, all_unionizes_path, experiment_ids)
+
+    nstructures = len(structure_ids)
+    nhemispheres = len(HEMISPHERE_IDS)
+    nexperiments = len(experiment_ids)
+
+    tables = xr.DataArray(np.zeros([nexperiments, nstructures, nhemispheres, 2]),
+                            dims=['experiment', 'structure', 'hemisphere', 'injection'],
+                            coords={'experiment': experiment_ids, 'structure': structure_ids,
+                                    'hemisphere': list(map(map_hemisphere_id, HEMISPHERE_IDS)), 'injection': [False, True]})
+    print(tables.coords)
+
+    for hid in HEMISPHERE_IDS:
+        for isinj in [0, 1]:
+            spec_table = get_specified_projection_table(mcc, unionizes, experiment_ids, structure_ids, isinj, hid, data_field_key)
+            print(hid, isinj, spec_table.values.sum())
+            tables.loc[:, :, map_hemisphere_id(hid), isinj] =  spec_table
+
+    print(tables.loc[482578964, 1066, 'left', 0])
+    print(tables.shape)
+    return tables
+
+
+def make_structure_paths_array():
+    structure_paths_array = np.zeros(projection_unionize.structure.shape+(ontology_depth,), dtype=projection_unionize.structure.dtype)
+    for i in range(projection_unionize.structure.shape[0]):
+        structure_id = int(projection_unionize.structure[i])
+        if structure_id > 0:
+            path = structure_paths[structure_id]
+            structure_paths_array[i,:len(path)] = path
+    return structure_paths_array
+
+
+def make_annotation_volume_paths():
+    ccf_anno_paths = np.zeros(ccf_anno.shape+(ontology_depth,), dtype=ccf_anno.dtype)
+    for i in range(ccf_anno.shape[0]):
+        for j in range(ccf_anno.shape[1]):
+            for k in range(ccf_anno.shape[2]):
+                structure_id = ccf_anno[i,j,k]
+                if structure_id > 0:
+                    path = structure_paths[structure_id]
+                    ccf_anno_paths[i,j,k,:len(path)] = path
+    return ccf_anno_paths
+
+
+def make_primary_structure_paths():
+    primary_structure_paths = np.zeros((len(primary_structures), ontology_depth), dtype=primary_structures.dtype)
+    for i in range(len(primary_structures)):
+        structure_id = int(primary_structures[i])
+        if structure_id > 0:
+            path = structure_paths[structure_id]
+            primary_structure_paths[i,:len(path)] = path
+    return primary_structure_paths
+
+
+def make_projection_volume():
+    for ii, eid in enumerate(experiment_ids):
+
+        data, header = mcc.get_projection_density(eid)
+        logging.info('read in data for experiment {0}'.format(eid))
+
+        if ii == 0:
+            dshape = list(data.shape)
+            volume = np.zeros(dshape + [len(experiment_ids)], dtype=np.float32)
+            logging.info('volume occupies {0} bytes ({1})'.format(volume.nbytes, volume.dtype.name))
+
+        volume[:, :, :, ii] = data
+        logging.info('pasted data from experiment {0} ({1})'.format(eid, ii))
+    return volume
+
+
+def make_injection_structures_arrays():
+    injection_structures_list = [[int(id) for id in s.split('/')] for s in experiments_ds.injection_structures.values.tolist()]
+    injection_structures_arr = np.zeros((len(injection_structures_list), max([len(x) for x in injection_structures_list])))
+    injection_structure_paths = np.zeros(injection_structures_arr.shape+(ontology_depth,), dtype=injection_structures_arr.dtype)
+    for i, structures in enumerate(injection_structures_list):
+        injection_structures_arr[i][:len(structures)] = structures
+        for j in range(len(structures)):
+            path = structure_paths[structures[j]]
+            injection_structure_paths[i,j,:len(path)] = path
+    return injection_structures_arr, injection_structure_paths
 
 
 def rgb_to_hex(rgb):
@@ -55,7 +170,7 @@ def get_structure_information(tree, summary_set_id=SUMMARY_SET_ID, exclude_from_
         structure in question.
     summary_structures : set of int
         Contains the ids of each summary structure (minus those specifically excluded)
-        
+
     '''
 
     nodes = pd.DataFrame(tree.nodes())
@@ -99,122 +214,22 @@ def main():
 
     ccf_anno = mcc.get_annotation_volume(file_name=os.path.join(mcc_dir, 'annotation_{:d}.nrrd'.format(args.resolution)))[0]
     ontology_depth = max([len(p) for p in structure_paths.values()])
-
-    def make_annotation_volume_paths():
-        ccf_anno_paths = np.zeros(ccf_anno.shape+(ontology_depth,), dtype=ccf_anno.dtype)
-        for i in range(ccf_anno.shape[0]):
-            for j in range(ccf_anno.shape[1]):
-                for k in range(ccf_anno.shape[2]):
-                    structure_id = ccf_anno[i,j,k]
-                    if structure_id > 0:
-                        path = structure_paths[structure_id]
-                        ccf_anno_paths[i,j,k,:len(path)] = path
-        return ccf_anno_paths
     ccf_anno_paths = make_annotation_volume_paths()
 
-    def make_unionize_tables(data_field_key):
-        HEMISPHERE_IDS = [1, 2, 3]
-        HEMISPHERE_MAP = {1: 'left', 2: 'right', 3: 'bilateral'}
-
-        def get_projection_table(unionizes, experiment_ids, structure_ids, data_field):
-            unionizes = unionizes.pivot(index='experiment_id', columns='structure_id', values=data_field)
-            unionizes = unionizes.reindex(index=experiment_ids, columns=structure_ids, fill_value=0.0)
-            unionizes = unionizes.fillna(0.0)
-            return unionizes
-
-        def get_specified_projection_table(mcc, unionizes, experiment_ids, structure_ids,
-                                           is_injection, hemisphere_id, data_field):
-            unionizes = mcc.filter_structure_unionizes(unionizes, is_injection=bool(is_injection),
-                                                       structure_ids=structure_ids, include_descendants=True,
-                                                       hemisphere_ids=[hemisphere_id])
-            return get_projection_table(unionizes, experiment_ids, structure_ids, data_field)
-
-        def get_all_unionizes(mcc, all_unionizes_path, experiment_ids):
-            try:
-                unionizes = pd.read_csv(all_unionizes_path)
-            except (IOError, ValueError) as err:
-                unionizes = mcc.get_structure_unionizes(experiment_ids)
-                unionizes.to_csv(all_unionizes_path)
-            return unionizes
-
-        def map_hemisphere_id(hem_id):
-           return HEMISPHERE_MAP[hem_id]
-
-        unionizes = get_all_unionizes(mcc, all_unionizes_path, experiment_ids)
-
-        nstructures = len(structure_ids)
-        nhemispheres = len(HEMISPHERE_IDS)
-        nexperiments = len(experiment_ids)
-
-        tables = xr.DataArray(np.zeros([nexperiments, nstructures, nhemispheres, 2]),
-                              dims=['experiment', 'structure', 'hemisphere', 'injection'],
-                              coords={'experiment': experiment_ids, 'structure': structure_ids,
-                                      'hemisphere': list(map(map_hemisphere_id, HEMISPHERE_IDS)), 'injection': [False, True]})
-        print(tables.coords)
-
-        for hid in HEMISPHERE_IDS:
-            for isinj in [0, 1]:
-                spec_table = get_specified_projection_table(mcc, unionizes, experiment_ids, structure_ids, isinj, hid, data_field_key)
-                print(hid, isinj, spec_table.values.sum())
-                tables.loc[:, :, map_hemisphere_id(hid), isinj] =  spec_table
-
-        print(tables.loc[482578964, 1066, 'left', 0])
-        print(tables.shape)
-        return tables
     pv = make_unionize_tables('projection_volume')
     npv = make_unionize_tables('normalized_projection_volume')
 
     projection_unionize = xr.concat([pv,npv],xr.DataArray([False,True],dims=['normalized'],name='normalized'))
     #projection_unionize['injection'] = xr.DataArray([False,True], dims=['injection'],name='injection')
     structure_volumes = make_unionize_tables('volume')
-   
-    def make_structure_paths_array():
-        structure_paths_array = np.zeros(projection_unionize.structure.shape+(ontology_depth,), dtype=projection_unionize.structure.dtype)
-        for i in range(projection_unionize.structure.shape[0]):
-            structure_id = int(projection_unionize.structure[i])
-            if structure_id > 0:
-                path = structure_paths[structure_id]
-                structure_paths_array[i,:len(path)] = path
-        return structure_paths_array
+
     structure_paths_array = make_structure_paths_array()
 
     primary_structures = experiments_ds.structure_id.values
-    def make_primary_structure_paths():
-        primary_structure_paths = np.zeros((len(primary_structures), ontology_depth), dtype=primary_structures.dtype)
-        for i in range(len(primary_structures)):
-            structure_id = int(primary_structures[i])
-            if structure_id > 0:
-                path = structure_paths[structure_id]
-                primary_structure_paths[i,:len(path)] = path
-        return primary_structure_paths
     primary_structure_paths = make_primary_structure_paths()
 
-    def make_projection_volume():
-        for ii, eid in enumerate(experiment_ids):
-
-            data, header = mcc.get_projection_density(eid)
-            logging.info('read in data for experiment {0}'.format(eid))
-
-            if ii == 0:
-                dshape = list(data.shape)
-                volume = np.zeros(dshape + [len(experiment_ids)], dtype=np.float32)
-                logging.info('volume occupies {0} bytes ({1})'.format(volume.nbytes, volume.dtype.name))
-
-            volume[:, :, :, ii] = data
-            logging.info('pasted data from experiment {0} ({1})'.format(eid, ii))
-        return volume
     volume = make_projection_volume()
 
-    def make_injection_structures_arrays():
-        injection_structures_list = [[int(id) for id in s.split('/')] for s in experiments_ds.injection_structures.values.tolist()]
-        injection_structures_arr = np.zeros((len(injection_structures_list), max([len(x) for x in injection_structures_list])))
-        injection_structure_paths = np.zeros(injection_structures_arr.shape+(ontology_depth,), dtype=injection_structures_arr.dtype)
-        for i, structures in enumerate(injection_structures_list):
-            injection_structures_arr[i][:len(structures)] = structures
-            for j in range(len(structures)):
-                path = structure_paths[structures[j]]
-                injection_structure_paths[i,j,:len(path)] = path
-        return injection_structures_arr, injection_structure_paths
     injection_structures_arr, injection_structure_paths = make_injection_structures_arrays()
 
     ccf_dims = ['anterior_posterior', 'superior_inferior', 'left_right']
@@ -233,6 +248,7 @@ def main():
             'experiment': experiment_ids,
             'structures': (['structure', 'depth'], structure_paths_array),
             'is_summary_structure': (['structure'], [structure in summary_structures for structure in projection_unionize.structure]),
+            'structure_color': structure_colors,
             'anterior_posterior': args.resolution*np.arange(ccf_anno.shape[0]),
             'superior_inferior': args.resolution*np.arange(ccf_anno.shape[1]),
             'left_right': args.resolution*np.arange(ccf_anno.shape[2]),
