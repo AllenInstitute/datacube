be83cc79bdd896ebe0b4efe2a96d737f7dd6135a
Chris Barber <chrisba@alleninstitute.org>
2018-08-15 15:20:22 -0700

    avoid huge `pd.read_csv` memory overhead ; remove unnecessary imports (#99)

--- scripts/datasets/human_mtg_transcriptomics.py
+++ scripts/datasets/human_mtg_transcriptomics.py
@@ -4,39 +4,17 @@ from __future__ import division
 import argparse
 import os
 import logging
-import json
-import urllib.parse
-import requests
-from io import StringIO
-import shutil
-from collections import OrderedDict
-import tempfile
-import psycopg2
-import csv
-from concurrent.futures import ThreadPoolExecutor
-import functools
-from functools import partial
-import types
 
-import nrrd
+import wget
+import zipfile
+
 import numpy as np
 import xarray as xr
 import pandas as pd
 import zarr
-import numcodecs
-import h5py
-import dask
-import dask.array as da
-
-from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
-from allensdk.config.manifest import Manifest
 
 
 def main():
-    import wget
-    import pandas as pd
-    import zipfile
-
     url = args.data_src + '/api/v2/well_known_file_download/694416044'
     logging.info('downloading matrix zipfile from {} to {}'.format(url, args.data_dir))
     filename = wget.download(url, out=args.data_dir, bar=lambda *args: None)
@@ -50,10 +28,16 @@ def main():
     df_rows = pd.read_csv(args.data_dir + 'human_MTG_2018-06-14_genes-rows.csv')
     logging.info('reading cols csv')
     df_cols = pd.read_csv(args.data_dir + 'human_MTG_2018-06-14_samples-columns.csv')
+
+    chunksize = 1000
     logging.info('reading exon-matrix csv')
-    df_exon = pd.read_csv(args.data_dir + 'human_MTG_2018-06-14_exon-matrix.csv')
+    exon_expression = np.empty((len(df_rows), len(df_cols)), dtype=np.int32)
+    for chunk in pd.read_csv(args.data_dir + 'human_MTG_2018-06-14_exon-matrix.csv', dtype=np.int32, chunksize=chunksize):
+        exon_expression[chunk.index[0]:(chunk.index[-1]+1),:] = chunk.values[:,1:]
     logging.info('reading intron-matrix csv')
-    df_intron = pd.read_csv(args.data_dir + 'human_MTG_2018-06-14_intron-matrix.csv')
+    intron_expression = np.empty((len(df_rows), len(df_cols)), dtype=np.int32)
+    for chunk in pd.read_csv(args.data_dir + 'human_MTG_2018-06-14_intron-matrix.csv', dtype=np.int32, chunksize=chunksize):
+        intron_expression[chunk.index[0]:(chunk.index[-1]+1),:] = chunk.values[:,1:]
 
     logging.info('merging into dataset')
     ds_rows = xr.Dataset.from_dataframe(df_rows).rename({'gene': 'gene_symbol', 'index': 'gene'})
@@ -61,8 +45,8 @@ def main():
     ds_cols = xr.Dataset.from_dataframe(df_cols).rename({'index': 'nucleus'})
     ds_cols.set_index(nucleus='sample_id', inplace=True)
     ds = xr.merge([ds_rows, ds_cols])
-    ds['exon_expression'] = xr.DataArray(df_exon.values[:,1:].astype(np.int32), dims=('gene', 'nucleus'))
-    ds['intron_expression'] = xr.DataArray(df_intron.values[:,1:].astype(np.int32), dims=('gene', 'nucleus'))
+    ds['exon_expression'] = xr.DataArray(exon_expression, dims=('gene', 'nucleus'))
+    ds['intron_expression'] = xr.DataArray(intron_expression, dims=('gene', 'nucleus'))
 
     store_file = os.path.join(args.data_dir, args.data_name + '.zarr.lmdb')
     store = zarr.storage.LMDBStore(store_file)
