196345ba230131f41c9e57f2a367c0e682b4676e
Chris Barber <chrisba@alleninstitute.org>
2018-09-06 12:32:55 -0700

    human mtg dataset: fix rma data source, add remaining foreign keys

--- scripts/datasets/human_mtg_transcriptomics.py
+++ scripts/datasets/human_mtg_transcriptomics.py
@@ -56,10 +56,14 @@ def main():
     ds['exon_expression'] = exon_expression
     ds['intron_expression'] = intron_expression
 
-    rma = RmaApi(base_uri='http://testwarehouse:9000/')
+    rma = RmaApi(base_uri=args.data_src)
     organism = rma.model_query(model='Organism', criteria="[name$il'homo sapiens']", num_rows='all')[0]
     ds['organism_id'] = xr.full_like(ds.organism, organism['id'], dtype=np.int)
     ds.organism[:] = organism['name']
+    reference_genome = rma.model_query(model='ReferenceGenome', criteria="[name$eq'GRCh38.p2'][build$eq'ref_GRCh38.p2_top_level'][organism_id$eq{}]".format(organism['id']), num_rows='all')[0]
+    genes = rma.model_query(model='Gene', criteria="[organism_id$eq{}][reference_genome_id$eq{}]".format(organism['id'], reference_genome['id']), only=['genes.id', 'genes.entrez_id'], num_rows='all')
+    genes = pd.DataFrame(genes).set_index('entrez_id').T.to_dict()
+    ds['gene_id'] = xr.DataArray([genes[entrez_id]['id'] for entrez_id in ds.gene.values], dims=ds.gene.dims)
     chromosomes = rma.model_query(model='Chromosome', criteria='[organism_id$eq{}]'.format(organism['id']), num_rows='all')
     chromosomes = pd.DataFrame(chromosomes).set_index('name').T.to_dict()
     ds['chromosome_id'] = xr.DataArray([chromosomes[chromosome_name]['id'] for chromosome_name in ds.chromosome.values], dims=ds.chromosome.dims)
@@ -68,11 +72,20 @@ def main():
     ds['age_id'] = xr.DataArray([ages[float(age_days) if age_days != 'unknown' else 0.]['id'] for age_days in ds.age_days.values], dims=ds.age_days.dims)
     hemispheres = rma.model_query(model='Hemisphere', num_rows='all')
     hemispheres = pd.DataFrame(hemispheres).set_index('symbol').T.to_dict()
-    ds['hemisphere_id'] = xr.DataArray([hemispheres[hemisphere_symbol]['id'] for hemisphere_symbol in ds.brain_hemisphere.values], dims=ds.brain_hemisphere.dims)
+    ds['brain_hemisphere_id'] = xr.DataArray([hemispheres[hemisphere_symbol]['id'] for hemisphere_symbol in ds.brain_hemisphere.values], dims=ds.brain_hemisphere.dims)
     ontology_id = rma.model_query(model='Ontology', criteria="[name$eq'Human Brain Atlas']", num_rows='all')[0]['id']
     structures = rma.model_query(model='Structure', criteria="[ontology_id$eq{}][hemisphere_id$eq{}]".format(ontology_id, hemispheres[None]['id']), num_rows='all')
     structures = pd.DataFrame(structures).set_index('acronym').T.to_dict()
-    ds['structure_id'] = xr.DataArray([structures[structure_acronym]['id'] for structure_acronym in ds.brain_region.values], dims=ds.brain_region.dims)
+    ds['brain_region_id'] = xr.DataArray([structures[structure_acronym]['id'] for structure_acronym in ds.brain_region.values], dims=ds.brain_region.dims)
+    cortical_layers_ontology_id = rma.model_query(model='Ontology', criteria="[abbreviation$eq'CellTypesHumanCorticalLayers'][organism_id$eq{}]".format(organism['id']), num_rows='all')[0]['id']
+    cortical_layers = rma.model_query(model='Structure', criteria="[ontology_id$eq{}][hemisphere_id$eq{}]".format(cortical_layers_ontology_id, hemispheres[None]['id']), num_rows='all')
+    cortical_layers = pd.DataFrame(cortical_layers).set_index('acronym').T.to_dict()
+    ds['brain_subregion_id'] = xr.DataArray([cortical_layers[layer_abbrev.replace('L', 'Layer')]['id'] for layer_abbrev in ds.brain_subregion.values], dims=ds.brain_subregion.dims)
+    mouse_organism_id = rma.model_query(model='Organism', criteria="[name$il'mus musculus']", num_rows='all')[0]['id']
+    mouse_reference_genome = rma.model_query(model='ReferenceGenome', criteria="[name$eq'GRCm38.p3'][build$eq'ref_GRCm38.p3_top_level'][organism_id$eq{}]".format(mouse_organism_id), num_rows='all')[0]
+    mouse_genes = rma.model_query(model='Gene', criteria="[organism_id$eq{}][reference_genome_id$eq{}]".format(mouse_organism_id, mouse_reference_genome['id']), only=['genes.id', 'genes.acronym'], num_rows='all')
+    mouse_genes = pd.DataFrame(mouse_genes).set_index('acronym').T.to_dict()
+    ds['mouse_homologene_ids'] = xr.DataArray([';'.join([str(mouse_genes[acronym]['id']) for acronym in homologenes.split(';')] if isinstance(homologenes, str) else []) for homologenes in ds.mouse_homologenes.values], dims=ds.mouse_homologenes.dims)
 
     store_file = os.path.join(args.data_dir, args.data_name + '.zarr.lmdb')
     store = zarr.storage.LMDBStore(store_file)
